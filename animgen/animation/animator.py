77ef467137e9c464d6ca141f5fcbd86fa2ac296a
KahnSvaer <Shivansh.pachnanda.work@gmail.com>
2026-08-15 19:57:29 +0530

    anim: initial animator structuring

---
+++ animgen/animation/animator.py
@@ -0,0 +1,72 @@
+from typing import Any, Callable, Protocol
+
+from animgen.core.armature import Armature
+from animgen.core.types import AnimationFrame, TimeSeconds
+
+
+class AnimationCallback(Protocol):
+    """
+    A Protocol that allows arbitrary typing subscription at runtime, e.g.,
+    `AnimationCallback[Armature, list[int], ...]`
+    which resolves to `Callable[..., Any]` for type checkers and at runtime.
+    """
+
+    def __call__(
+        self,
+        armature: Armature,
+        list_bones: list[int],
+        *args: Any,
+        **kwargs: Any,
+    ) -> Any: ...
+
+    def __class_getitem__(cls, params: Any) -> Any:
+        return Callable[..., Any]
+
+
+class AnimationClip:
+    """
+    Would be responsible for aggregating multiple motion clips into a single animation
+    """
+
+    def __init__(
+        self,
+        name: str,
+        duration: float,
+        armature: Armature,
+        is_loopable: bool = False,
+    ):
+        self.name: str = name
+        self.duration: TimeSeconds = duration
+        self.armature: Armature = armature
+        self.is_loopable: bool = is_loopable
+
+        self.positions: dict[TimeSeconds, AnimationFrame] = {}
+
+    def add_animation_movements(
+        self,
+    ):
+        pass
+
+    def check_loopable(
+        self,
+    ) -> None:
+        # Essentially check if the last posvector is equal to starting pose vector
+        # WOuld change the value of self.loopable in place
+        pass
+
+
+class Animator:
+    """
+    Manages procedural animations on armatures.
+    Supports registering animations, tracking them, and evaluating them.
+    """
+
+    def __init__(self) -> None:
+        self.animations: dict[str, AnimationClip] = {}
+        self.animation_lists: dict[str, AnimationClip] = {}
+
+    def add_animation_clip(self, animation_clip: AnimationClip) -> None:
+        """
+        Adds an animation clip to the animator.
+        """
+        self.animations[animation_clip.name] = animation_clip
--- animgen/core/types.py
+++ animgen/core/types.py
@@ -4,6 +4,5 @@ from jaxtyping import Float
 Vec3 = tuple[float, float, float]
 PoseTransforms = Float[torch.Tensor, "4 4"]
 Vector3Tensor = Float[torch.Tensor, "3"]
-AnimationFrame = list[
-    Vec3
-]  # Position of all joints in the armature, index correspond to armature bones list property
+AnimationFrame = list[Vec3]
+TimeSeconds = float
